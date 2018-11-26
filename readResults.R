ba1b5cc7cb737ed473825a5fce3d6548821a2f47
Yoshiki <kopites_1892@yahoo.co.jp>
2018-11-26 11:58:49 +0900

    read results from last session

---
+++ readResults.R
@@ -0,0 +1,22 @@
+
+ARlags <- readRDS("results/ARlags.rds")
+VARlags <- readRDS("results/VARlags.rds")
+
+DIfactor <- readRDS("results/DIfactor.rds")
+DIfactorDyn <- readRDS("results/DIfactorDyn.rds")
+DIfactorR2 <- readRDS("results/DIfactorR2.rds")
+DIlags <- readRDS("results/DIlags.rds")
+
+LASSOcoefs <- readRDS("results/LASSOcoefs.rds")
+LASSOlambda <- readRDS("results/LASSOlambda.rds")
+LASSOsparsityRatio <- readRDS("results/LASSOsparsityRatio.rds")
+
+LASSO2coefs <- readRDS("results/LASSO2coefs.rds")
+LASSO2lambda <- readRDS("results/LASSO2lambda.rds")
+LASSO2sparsityRatio <- readRDS("results/LASSO2sparsityRatio.rds")
+
+ENETcoefs <- readRDS("results/ENETcoefs.rds")
+ENETalpha <- readRDS("results/ENETalpha.rds")
+ENETsparsityRatio <- readRDS("results/ENETsparsityRatio.rds")
+
+MSFEs <- readRDS("results/MSFEs.rds")
