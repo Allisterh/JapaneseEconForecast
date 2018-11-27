895e78549023c8a8cdcd9c3c64f678ce63071061
Yoshiki Nakajima <kopites_1892@yahoo.co.jp>
2018-11-27 12:51:09 +0900

    Revert "parallelised"
    This reverts commit ad44d03dd688e1edc0667160a1a21c6f7a75533f.

--- models/glasso.r
+++ models/glasso.r
@@ -24,62 +24,51 @@ for (i in 1:12){
 y <- dat[, targetVar] %>% 
   set_colnames("y")
 X <- lag.xts(dat, 1:12+h-1)
-lambdaChoises <- 10^(seq(-3,0,len=10)) # lambda choices, selection on CV
-# lambdaChoises<- c(3,1,0.3,0.1,0.03,0.01,0.003,0.001, 0.0003, 0.0001)
+# lambdaChoises <- 10^(seq(-2,0,len=100)) # lambda choices, selection on CV
+lambdaChoises<- c(1,0.3,0.1,0.03,0.01,0.003,0.001)
 predErr <- matrix(NA, nrow=length(lambdaChoises), ncol=winSize) 
 
 
-# pb <- txtProgressBar(0, winSize, style=3)
+pb <- txtProgressBar(0, winSize, style=3)
 tictoc::tic()
 
-predErr <-
-  foreach(t = 1:winSize, .combine = "cbind") %dopar% { # penalty param 3.5 hrs wo parallelising
-    fitGLasso <- grplasso(X[(12+h):T1+t-1,],y[(12+h):T1+t-1], idx, model=LinReg(),
-                          lambda = lambdaChoises, center = F, standardize = F, control=grpl.control(trace=0))
-    predGLasso <- predict(fitGLasso, newdata=X[T1+t,])
-    as.numeric((predGLasso-as.numeric(y[T1+t,]))^2)
-    # setTxtProgressBar(pb,t)
-  }
-tictoc::toc()
-
+for (t in 1:winSize){ # penalty param / lag selection
+  
+  fitGLasso <- grplasso(X[(12+h):T1+t-1,],y[(12+h):T1+t-1], idx, model=LinReg(),
+                        lambda = lambdaChoises, center = F, standardize = F)
+  predGLasso <- predict(fitGLasso, newdata=X[T1+t,])
+  predErr[,t] <- as.numeric((predGLasso-as.numeric(y[t+1,]))^2)
+  setTxtProgressBar(pb,t)
+}
 cv <- apply(predErr,1,mean)
 optLam <- lambdaChoises[which.min(cv)]
 
-gLASSOlambda[horizon,targetVar] <- optLam
 
-
-# lamIdx <- which(cv==min(cv), arr.ind = T)[1] # optimal lambda (idx)
-# optLam <- lambdaChoises[lamIdx]
-# optLag <- which(cv==min(cv), arr.ind = T)[2] # opt lag
+lamIdx <- which(cv==min(cv), arr.ind = T)[1] # optimal lambda (idx)
+optLam <- lambdaChoises[lamIdx]
+optLag <- which(cv==min(cv), arr.ind = T)[2] # opt lag
+tictoc::toc()
 
 ### evaluation
-# Now we have selected optimal regularisation parameter based on cross valisation. 
-coefTracker <- matrix(NA, nrow=winSize, ncol=ncol(X))
-predErrGLasso <- numeric()
-# pb<-txtProgressBar(0, winSize, style=3)
-for (t in 1:winSize){ # forecast evaluation
-  fitGLasso <- grplasso(X[(T1+1):T2+t-1,], y[(T1+1):T2+t-1], idx, model=LinReg(),
+# Now we have selected optimal regularisation parameter and lag length based on cross valisation. 
+
+x <- lag.xts(dat, 1:optLag)
+x <- x[-c(1:optLag),] # trim x'NAs **Note: I have to remove last obs if h > 1 bc there's no counterpart obs for y**
+y <- lag.xts(dat[,51], -(h-1)) # **Note: remove last (h-1) obs's when h >1 **
+y <- y[-c(1:optLag),]
+idx<- rep(c(rep(1,g1), rep(2,g2-g1), rep(3,g3-g2), rep(4,g4-g3), 
+            rep(5,g5-g4), rep(6,g6-g5),rep(7,g7-g6), rep(8,g8-g7), 
+            rep(9,g9-g8), rep(10,g10-g9), rep(11,g11-g10), rep(12,g12-g11)),optLag)
+
+
+predErrGLasso <- 0 
+pb<-txtProgressBar(T2-1, end-1, style=3)
+for (t in T2:(end-1)){ # forecast evaluation
+  fitGLasso <- grplasso(x, y, idx, model=LinReg(),
                         lambda = optLam, center = F, standardize = F)
-  predGLasso <- predict(fitGLasso, newdata=X[T2+t,])
-  predErrGLasso[t] <- as.numeric((predGLasso - as.numeric(y[T2+t,]))^2)
-  coefTracker[t,] <- as.numeric(fitGLasso$coef)
-  # setTxtProgressBar(pb,t)
+  predGLasso <- predict(fitGLasso, newdata=x[t+1,])
+  predErrGLasso <- as.numeric(predLasso - as.numeric(y[t+1,]))^2 + predErrGLasso
+  setTxtProgressBar(pb,t)
 }
-msfeGLasso <- mean(predErrGLasso)
-
-coefTracker[coefTracker < 1e-7] <- 0
-coefTracker[coefTracker >=1e-7] <- 1 # 1 if coef is selected (non-zero)
-
-# save results
-gLASSOsparsityRatio[horizon,targetVar] <- sum(coefTracker)/length(coefTracker) # the ratio of non-zero coef
-
-if (horizon == 1) {gLASSOcoefs[[var]] <- list()} # initialise by setting sub-list so that each main list contains sub-lists
-gLASSOcoefs[[var]][[horizon]] <- coefTracker
-if (horizon == 4) {names(gLASSOcoefs[[var]]) <- paste("h", hChoises, sep="")}
-
-MSFEs[[horizon]]["gLASSO", targetVar] <- msfeGLasso
-
-rm(output, employment, sales, consumption, housing, inventory, stock,
-   exchange, interest, money,price, idx,idxList,i,j) 
-
+msfeGLasso <- predErrGLasso/(end-T2) 
 
