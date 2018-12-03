bd757ab53aa658de960f1fac37dbc0fd36a0b866
Yoshiki Nakajima <kopites_1892@yahoo.co.jp>
2018-12-03 12:16:45 +0900

    add DIfactorList

--- readResults.R
+++ readResults.R
@@ -6,6 +6,7 @@ DIfactor <- readRDS("results/DIfactor.rds")
 DIfactorDyn <- readRDS("results/DIfactorDyn.rds")
 DIfactorR2 <- readRDS("results/DIfactorR2.rds")
 DIlags <- readRDS("results/DIlags.rds")
+DIfactorList <- readRDS("results/DIfactorList.rds")
 
 LASSOcoefs <- readRDS("results/LASSOcoefs.rds")
 LASSOlambda <- readRDS("results/LASSOlambda.rds")
@@ -19,8 +20,8 @@ ENETcoefs <- readRDS("results/ENETcoefs.rds")
 ENETalpha <- readRDS("results/ENETalpha.rds")
 ENETsparsityRatio <- readRDS("results/ENETsparsityRatio.rds")
 
-gLASSOcoefs <- readRDS("results/gLASSOcoefs") # listed by variable and horizon, each list window x horizon
-gLASSOlambda <- readRDS("results/gLASSOlambda")
-gLASSOsparsityRatio <- readRDS("results/gLASSOsparsityRatio")
+# gLASSOcoefs <- readRDS("results/gLASSOcoefs") # listed by variable and horizon, each list window x horizon
+# gLASSOlambda <- readRDS("results/gLASSOlambda")
+# gLASSOsparsityRatio <- readRDS("results/gLASSOsparsityRatio")
 
 MSFEs <- readRDS("results/MSFEs.rds")
