65742bb15c789992dbeb4f25c65b1d40c409e72e
Yoshiki Nakajima <kopites_1892@yahoo.co.jp>
2018-12-03 12:18:15 +0900

    Change lambda option

--- models/glasso.r
+++ models/glasso.r
@@ -28,39 +28,63 @@ X <- lag.xts(dat, 1:12+h-1)
 
 # cross validation --------------------------------------------------------
 
-lambdaChoises <- 10^(seq(-2,0,len=100))
+# lambdaChoises <- 10^(seq(3,-2,len=10))
+tictoc::tic()
+lambdaChoises <- 50:1
+
+# predErr <- matrix(NA, ncol=winSize, nrow=length(lambdaChoises))
+
+# for (t in 1:winSize){ # penalty param / lag selection
+#   print(paste("-------- t =", t, "-------"))
+#   fitGLasso <- grplasso(X[(12+h):T1+t-1,],y[(12+h):T1+t-1], idx, model=LinReg(), # 1 min
+#                         lambda = lambdaChoises, center = F, standardize = F, 
+#                         control = grpl.control(max.iter=1e07, tol=1e-15, trace=1))
+#   predGLasso <- predict(fitGLasso, newdata=X[T1+t,])
+#   predErr[,t] <- as.numeric((predGLasso-as.numeric(y[T1+t,]))^2)
+# }
+
 predErr <-  # 30min
-  foreach(t=1:winSize, .combine = "cbind", .inorder = F) %dopar% { # penalty param / lag selection
-    fitGLasso <- grplasso(X[(12+h):T1+t-1,],y[(12+h):T1+t-1], idx, model=LinReg(), # 1 min
-                          lambda = lambdaChoises, center = F, standardize = F, control = grpl.control(trace=0))
-    predGLasso <- predict(fitGLasso, newdata=X[T1+t,])
-    as.numeric((predGLasso-as.numeric(y[T1+t,]))^2)
-  }
+foreach(t=1:winSize, .combine = "cbind", .inorder = F) %dopar% { # penalty param / lag selection
+  fitGLasso <- grplasso(X[(12+h):T1+t-1,],y[(12+h):T1+t-1], idx, model=LinReg(), # 1 min
+                        lambda = lambdaChoises, center = F, standardize = F,
+                        control = grpl.control(max.iter=1e07, tol=1e-15, trace=1))
+  predGLasso <- predict(fitGLasso, newdata=X[T1+t,])
+  as.numeric((predGLasso-as.numeric(y[T1+t,]))^2)
+}
 cv <- apply(predErr,1,mean)
 optLam <- lambdaChoises[which.min(cv)]
 gLASSOlambda[horizon,targetVar] <- optLam # save optimal lambda
-
+tictoc::toc()
 
 # evaluation --------------------------------------------------------------
 
-predErr <- numeric()
-coefTracker <- matrix(NA, nrow=winSize, ncol=ncol(X))
+# predErr <- numeric()
+# coefTracker <- matrix(NA, nrow=winSize, ncol=ncol(X))
+# for (t in 1:winSize){ # forecast evaluation, 45 sec
+#   fitGLasso <- grplasso(X[(T1+1):T2+t-1,], y[(T1+1):T2+t-1], idx, model=LinReg(),
+#                         lambda = optLam, center = F, standardize = F, 
+#                         control = grpl.control(max.iter=1e07, tol=1e-07,trace=0))
+#   predGLasso <- predict(fitGLasso, newdata=X[T2+t,])
+#   predErr[t] <- as.numeric(predGLasso - y[T2+t,])^2
+#   coefTracker[t,] <- fitGLasso$coef
+# }
 
-eval <- foreach(t = 1:winSize) %dopar% { # forecast evaluation, 45 sec
-  fitGLasso <- grplasso(X[(T1+1):T2+t-1,], y[(T1+1):T2+t-1], idx, model=LinReg(),
-                        lambda = optLam, center = F, standardize = F, control = grpl.control(trace=0))
-  predGLasso <- predict(fitGLasso, newdata=X[T2+t,])
-  err <- as.numeric(predGLasso - y[T2+t,])^2
-  coefs <- fitGLasso$coef
-  list(err, coefs)
-}
+eval <-
+  foreach(t = 1:winSize) %dopar% { # forecast evaluation, 45 sec
+    fitGLasso <- grplasso(X[(T1+1):T2+t-1,], y[(T1+1):T2+t-1], idx, model=LinReg(),
+                          lambda = optLam, center = F, standardize = F,
+                          control = grpl.control(max.iter=1e07, tol=1e-07,trace=0))
+    predGLasso <- predict(fitGLasso, newdata=X[T2+t,])
+    err <- as.numeric(predGLasso - y[T2+t,])^2
+    coefs <- fitGLasso$coef
+    list(err, coefs)
+  }
 predErr <- unlist(sapply(eval, function(foo) foo[1]))
 coefTracker <- matrix(unlist(sapply(eval, function(foo) foo[2])),
                       nrow=winSize, ncol=ncol(X), byrow=T)
 
 coefTracker[abs(coefTracker) < 1e-7] <- 0
 coefTracker[abs(coefTracker) >=1e-7] <- 1 # 1 if coef is selected (non-zero)
-
 
 # save results ------------------------------------------------------------
 
