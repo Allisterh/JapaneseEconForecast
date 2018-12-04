3d19ee8d8b316eca2388497e11a8b4f1bfff5ea6
Yoshiki Nakajima <kopites_1892@yahoo.co.jp>
2018-12-04 18:33:07 +0900

    widen lambda options

--- models/enet.r
+++ models/enet.r
@@ -5,11 +5,10 @@
 y <- dat[, targetVar] %>% 
   set_colnames("y")
 
-lambdaChoises <- 10^(seq(1,-3,len=100)) # lambda choices (or use the optLam from LASSO to reduce time)
-# lambdaChoises <- LASSOlambda[horizon,var]
-alphaChoises <- seq(0.01, 0.99, 0.01) 
+lambdaChoises <- 10^(seq(1.5,-3,len=100)) # lambda choices (or use the optLam from LASSO to reduce time)
+
+alphaChoises <- seq(0.01, 0.99, 0.02) 
 predErrEnet <- matrix(NA, nrow=length(lambdaChoises), ncol=winSize)
-# predErrEnet <- numeric() # in case we use the same lambda as LASSO
 X <- lag.xts(dat, 1:12+h-1)
 
 # cv  ---------------------------------------------------------------------
@@ -17,39 +16,36 @@ X <- lag.xts(dat, 1:12+h-1)
 cvScore <-  # nr-of-lambda x nr-of-alpha
   foreach(a = 1:length(alphaChoises), .combine = "cbind", .inorder = F) %dopar% {
     for (t in 1:winSize){
-      fitEnet <- glmnet(X[(12+h):T1+t-1,],y[(12+h):T1+t-1], # (p+h):T1 instead of 1:T1 bc first p obs's are missing
+      fitEnet <- glmnet::glmnet(X[(12+h):T1+t-1,],y[(12+h):T1+t-1], # (p+h):T1 instead of 1:T1 bc first p obs's are missing
                         lambda=lambdaChoises, family="gaussian", alpha=alphaChoises[a],
                         standardize=F, intercept=F, thresh=1e-15, maxit=1e07)
-      predEnet <- predict.glmnet(fitEnet, coredata(X[T1+t,]))
+      predEnet <- glmnet::predict.glmnet(fitEnet, zoo::coredata(X[T1+t,]))
       predErrEnet[,t] <- as.numeric((predEnet - as.numeric(y[T1+t]))^2)
   }
   apply(predErrEnet,1,mean) # MSE for each lambda in cross validation period (with fixed alpha)
 }
 
-# opts <- which(cvScore==min(cvScore), arr.ind = T) # 2D-indices for optimal lambda[1] and alpha[2]
-# optLambda <- lambdaChoises[opts[1]]
-# optAlpha <- alphaChoises[opts[2]]
 
+opts <- which(cvScore==min(cvScore), arr.ind = T) # 2D-indices for optimal lambda[1] and alpha[2]
+optLambda <- lambdaChoises[opts[1]]
+optAlpha <- alphaChoises[opts[2]]
 
-# cvScore <- ENETcv[[var]][[horizon]]
-
-optLambda <- lambdaChoises[which.min(apply(cvScore,1,mean))]
-optAlpha <- alphaChoises[which.min(apply(cvScore,2,mean))]
+# # Alternatively, you can take the average of cv-err across lambda[alpha] options and take the minimum avg value
+# optLambda <- lambdaChoises[which.min(apply(cvScore,1,mean))]
+# optAlpha <- alphaChoises[which.min(apply(cvScore,2,mean))]
 
 ENETlambda[horizon, targetVar] <- optLambda
 ENETalpha[horizon, targetVar] <- optAlpha
 
 
-
-
-# evaluatoin
+# forecast evaluation -----------------------------------------------------
 
 eval <- 
   foreach(t = 1:winSize) %dopar% { 
-    fit <- glmnet(X[(T1+1):T2+t-1,], y[(T1+1):T2+t-1], lambda = optLambda,
+    fit <- glmnet::glmnet(X[(T1+1):T2+t-1,], y[(T1+1):T2+t-1], lambda = optLambda,
                        family = "gaussian", alpha = optAlpha, standardize = F, intercept=F,
                        thresh=1e-15, maxit = 1e07)
-    pred <- predict.glmnet(fit, coredata(X[T2+t,]))
+    pred <- glmnet::predict.glmnet(fit, zoo::coredata(X[T2+t,]))
     err <- as.numeric((pred - y[T2+t])^2)
     coefs <- as.numeric(fit$beta)
     list(err, coefs)
@@ -62,8 +58,8 @@ coefTracker <- matrix(unlist(sapply(eval, function(foo) foo[2])),
 msfeEnet <- mean(predErr)
 
 # interpretation
-coefTracker[abs(coefTracker) < 1e-10] <- 0
-coefTracker[abs(coefTracker) >=1e-10] <- 1 # 1 if coef is selected (non-zero)
+coefTracker[abs(coefTracker) == 0] <- 0
+coefTracker[abs(coefTracker) != 0] <- 1 # 1 if coef is selected (non-zero)
 ENETsparsityRatio[horizon,targetVar] <- mean(coefTracker) # the ratio of non-zero coef
 ENETnonzero[horizon,targetVar] <- sum(coefTracker)/winSize # number of non-zero param's
 # Notice that the final object `Enetcoefs` is a list of lists (main list of variables and sub-list of horizons)
@@ -78,5 +74,5 @@ if (horizon == 4) {
 MSFEs[[horizon]]["ENET", targetVar] <- msfeEnet
 
 rm(coefTracker, X, y, cvScore,alphaChoises, predErrEnet,optLambda,
-   msfeEnet, optAlpha, lambdaChoises,eval)
+   msfeEnet, optAlpha, lambdaChoises,eval, opts)
 
