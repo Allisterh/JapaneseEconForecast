34e3cd147db7680353fc36b15208b079498210c0
Yoshiki Nakajima <kopites_1892@yahoo.co.jp>
2018-12-04 14:57:56 +0900

    nests AR

--- models/lasso2.r
+++ models/lasso2.r
@@ -1,6 +1,7 @@
 # lasso with regularisation parameter selected by cv, rolling window. Lag = 12
-# lasso.r + smaller penalty on auto-lag
-
+# lasso.r + nest AR + smaller penalty for autolag
+## Lags of dependent variable up to the same length as AR is included in LASSO w/o penalty
+## Lags of dep var not included in AR gets smaller penalty when included 
 
 # cross validation (b/w period T1 n T2) -----------------------------------
 
@@ -10,10 +11,13 @@ lambdaChoises <- 10^(seq(0,-3,len=100))
 
 X <- lag.xts(dat,1:12+h-1)
 pFac <- rep(1,ncol(X)) # penalty factor (control the effect of lambda)
-pFac[grep(targetVar, names(X))] <- 0.1 # penalise less if autolag
+pFac[grep(paste("\\b",targetVar, "\\b", sep=""),names(X))] <- 0.1 # penalise less if autolag
+
 
 predErrLasso <- 
   foreach(t = 1:winSize, .combine = "cbind") %dopar% {
+    lagAR <- ARlags[[var]][horizon,t] # get the result from AR
+    pFac[grep(targetVar, names(X))[1:lagAR]] <- 0  # pen factor = 0 if used in AR (nest AR into LASSO)
     fitLasso <- glmnet::glmnet(X[(12+h):T1+t-1,],y[(12+h):T1+t-1], # (p+h):T1 instead of 1:T1 bc first p obs's are missing
                        lambda=lambdaChoises, family="gaussian", alpha=1, penalty.factor=pFac,
                        standardize=F, intercept=F, thresh=1e-15, maxit=1e07)
@@ -29,6 +33,8 @@ LASSO2lambda[horizon,targetVar] <- optLam
 # forecast evaluation -----------------------------------------------------
 
 eval <- foreach(t = 1:winSize) %dopar% { 
+  lagAR <- ARlags[[var]][horizon,t]
+  pFac[grep(targetVar, names(X))[1:lagAR]] <- 0 
   fitLasso <- glmnet::glmnet(X[(T1+1):T2+t-1,], y[(T1+1):T2+t-1], lambda = optLam,
                      family = "gaussian", alpha = 1, standardize = F, intercept=F,
                      thresh=1e-15, maxit = 1e+07, penalty.factor = pFac)
@@ -56,5 +62,5 @@ if (horizon == 4) {names(LASSO2coefs[[var]]) <- paste("h", hChoises, sep="")}
 
 MSFEs[[horizon]]["LASSO2", targetVar] <- msfeLasso
 
-rm(coefTracker,X,x, y, cvScore,lambdaChoises,msfeLasso, optLam,predErrLasso,eval)
+rm(coefTracker,X, y, cvScore,lambdaChoises,msfeLasso, optLam,predErrLasso,eval, pFac)
 
