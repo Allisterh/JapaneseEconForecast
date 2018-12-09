f9bef7a1fc582a420c0c4ed42ef20e9edfcf3798
Yoshiki Nakajima <kopites_1892@yahoo.co.jp>
2018-12-09 10:10:01 +0900

    make codes tidier
    long list of `saveRDS`s to separate files

--- execution.R
+++ execution.R
@@ -4,22 +4,20 @@
 
 library(xts)
 library(magrittr)
-library(vars)
+library(tidyverse)
 library(glmnet)
 library(grplasso)
-
 library(foreach)
 library(doParallel)
 registerDoParallel(detectCores())
+library(openxlsx)
 
 
 # rm(list=ls())
 dat <- readRDS("data/dat.rds")
 hChoises <- c(1,3,6,12)
-targetVariables <- scan("txt/targetVariables.txt", character())
+targetVariables <- scan("txt/targetVariables.txt", character(), quiet=T)
 winSize <- 60
-# source("readResults.R") # load previously saved result
-
 # source("placeholders.r") # load placeholders to store results
 
 # model execution ---------------------------------------------------------
@@ -32,16 +30,12 @@ for (horizon in 1:length(hChoises)){
   T2 <- which(index(dat)==" 7 2013") - h # end of cv
   for(var in 1:length(targetVariables)){
     targetVar <- targetVariables[var]
-    
     # source("models/ar.r") # 7 mins to execute
-    # source("models/di.r") # 15 mins
-    # source("models/lasso.r") # 30 mins to execute
-    # source("models/lasso2.r")
-    source("models/lasso3.r") # 20 mins (lab)
-    source("models/lasso4.r") # 20 mins (lab)
+    # source("models/di.r") # 10 mins
+    source("models/dilasso.r") # 2 mins
+    # source("models/lasso.r") # 20 mins (lab)
     # source("models/enet.r") # 5 hrs (Lab)
     # source("models/glasso.r") # 5-6hrs (iMac)
-    
     setTxtProgressBar(pb, (horizon-1)*length(targetVariables)+var)
   }
 }
@@ -54,23 +48,26 @@ names(ARlagsCV) <- targetVariables
 names(VARlags) <- targetVariables
 names(DIlags) <- targetVariables
 names(DIfactorList) <- c(paste("h", hChoises, sep=""))
+names(DILASSOcoefs) <- targetVariables
 names(LASSOcoefs) <- targetVariables
-names(LASSO2coefs) <- targetVariables
-names(LASSO3coefs) <- targetVariables
-names(LASSO4coefs) <- targetVariables
 names(ENETcoefs) <- targetVariables
 names(ENETcv) <- targetVariables
 names(gLASSOcoefs) <- targetVar
 
 
+# comparison ---------------------------------------------------------------
 
-# interpret ---------------------------------------------------------------
+## standardise to AR
+MSFEs$h1 <- t(apply(MSFEs$h1, 1, function(x) x*(1/MSFEs$h1["AR",])))
+MSFEs$h3 <- t(apply(MSFEs$h3, 1, function(x) x*(1/MSFEs$h3["AR",])))
+MSFEs$h6 <- t(apply(MSFEs$h6, 1, function(x) x*(1/MSFEs$h6["AR",])))
+MSFEs$h12 <- t(apply(MSFEs$h12, 1, function(x) x*(1/MSFEs$h12["AR",])))
 
-models <- c("AR","DI","DILASSO","LASSO","LASSO2","LASSO3","LASSO4","ENET","gLASSO")
-Results <- list() # count the number outperformed (20 if outperformed in all the variables)
+## compare the performances of models against each other (How many times does each model outperform the counterparts)
+models <- c("AR","DI","DILASSO","LASSO","ENET","gLASSO")
+Results <- list()
 for (k in 1:4){
-  results  <- matrix(NA, length(models), length(models),
-                   dimnames = list(models, models))
+  results  <- matrix(NA, length(models), length(models), dimnames = list(models, models))
   for (i in 1:length(models)) {
     for (j in 1:length(models)) {
       results[i,j] <- sum(MSFEs[[k]][i,] < MSFEs[[k]][j,])
@@ -81,67 +78,10 @@ for (k in 1:4){
 }
 
 
-# MSFEs$h1 <- t(apply(MSFEs$h1, 1, function(x) x*(1/MSFEs$h1["AR",])))
-# MSFEs$h3 <- t(apply(MSFEs$h3, 1, function(x) x*(1/MSFEs$h3["AR",])))
-# MSFEs$h6 <- t(apply(MSFEs$h6, 1, function(x) x*(1/MSFEs$h6["AR",])))
-# MSFEs$h12 <- t(apply(MSFEs$h12, 1, function(x) x*(1/MSFEs$h12["AR",])))
 
-
-# store results -----------------------------------------------------------
-
-saveRDS(ARlags, "results/ARlags.rds")
-saveRDS(VARlags, "results/VARlags.rds")
-
-saveRDS(DIfactor, "results/DI/DIfactor.rds")
-saveRDS(DIfactorDyn, "results/DI/DIfactorDyn.rds")
-saveRDS(DIfactorR2, "results/DI/DIfactorR2.rds")
-saveRDS(DIlags,"results/DI/DIlags.rds")
-saveRDS(DIfactorList, "results/DI/DIfactorList.rds")
-
-saveRDS(LASSOcoefs, "results/LASSO/LASSOcoefs.rds")
-saveRDS(LASSOlambda, "results/LASSO/LASSOlambda.rds")
-saveRDS(LASSOsparsityRatio, "results/LASSO/LASSOsparsityRatio.rds")
-saveRDS(LASSOnonzero, "results/LASSO/LASSOnonzero.rds")
-
-saveRDS(LASSO2coefs, "results/LASSO2/LASSO2coefs.rds")
-saveRDS(LASSO2lambda, "results/LASSO2/LASSO2lambda.rds")
-saveRDS(LASSO2sparsityRatio, "results/LASSO2/LASSO2sparsityRatio.rds")
-saveRDS(LASSO2nonzero, "results/LASSO2/LASSO2nonzero.rds")
-
-saveRDS(LASSO3coefs, "results/LASSO3/LASSO3coefs.rds")
-saveRDS(LASSO3lambda, "results/LASSO3/LASSO3lambda.rds")
-saveRDS(LASSO3sparsityRatio, "results/LASSO3/LASSO3sparsityRatio.rds")
-saveRDS(LASSO3nonzero, "results/LASSO3/LASSO3nonzero.rds")
-
-saveRDS(LASSO4coefs, "results/LASSO4/LASSO4coefs.rds")
-saveRDS(LASSO4lambda, "results/LASSO4/LASSO4lambda.rds")
-saveRDS(LASSO4sparsityRatio, "results/LASSO4/LASSO4sparsityRatio.rds")
-saveRDS(LASSO4nonzero, "results/LASSO4/LASSO4nonzero.rds")
-
-saveRDS(ENETcoefs, "results/ENET/ENETcoefs.rds")
-saveRDS(ENETalpha, "results/ENET/ENETalpha.rds")
-saveRDS(ENETlambda, "results/ENET/ENETlambda.rds")
-saveRDS(ENETsparsityRatio, "results/ENET/ENETsparsityRatio.rds")
-saveRDS(ENETcv, "results/ENET/ENETcv.rds")
-saveRDS(ENETnonzero, "results/ENET/ENETnonzero.rds")
-
-saveRDS(ENET2coefs, "results/ENET/ENETcoefs.rds")
-saveRDS(ENET2alpha, "results/ENET/ENETalpha.rds")
-saveRDS(ENET2lambda, "results/ENET/ENETlambda.rds")
-saveRDS(ENET2sparsityRatio, "results/ENET/ENETsparsityRatio.rds")
-saveRDS(ENET2cv, "results/ENET/ENETcv.rds")
-saveRDS(ENET2nonzero, "results/ENET/ENETnonzero.rds")
-
-saveRDS(gLASSOcoefs, "results/gLASSO/gLASSOcoefs.rds")
-saveRDS(gLASSOlambda, "results/gLASSO/gLASSOlambda.rds")
-saveRDS(gLASSOsparsityRatio, "results/gLASSO/gLASSOsparsityRatio.rds")
-saveRDS(gLASSOnonzero, "results/gLASSO/gLASSOnonzero.rds")
-
-saveRDS(gLASSO2coefs, "results/gLASSO/gLASSOcoefs.rds")
-saveRDS(gLASSO2lambda, "results/gLASSO/gLASSOlambda.rds")
-saveRDS(gLASSO2sparsityRatio, "results/gLASSO/gLASSOsparsityRatio.rds")
-saveRDS(gLASSO2nonzero, "results/gLASSO/gLASSOnonzero.rds")
-
-saveRDS(MSFEs, "results/MSFEs.rds")
-
+# save results ------------------------------------------------------------
+source("saveResults.r") # saves outputs as rds
+write.xlsx(MSFEs, "results/excel/MSFEs.xlsx", rowNames=T)
+write.xlsx(Results, "results/excel/Results.xlsx", rowNames=T,keepNA=T)
+write.xlsx(DILASSOcoefs[[15]], "results/excel/DILASSOcoefs.xlsx", rowNames=T, keepNA=T)
 
---
+++ saveResults.r
@@ -0,0 +1,33 @@
+saveRDS(ARlags, "results/ARlags.rds")
+
+saveRDS(DIfactor, "results/DI/DIfactor.rds")
+saveRDS(DIfactorDyn, "results/DI/DIfactorDyn.rds")
+saveRDS(DIfactorR2, "results/DI/DIfactorR2.rds")
+saveRDS(DIlags,"results/DI/DIlags.rds")
+saveRDS(DIfactorList, "results/DI/DIfactorList.rds")
+saveRDS(DIinterpret, "results/DI/DIinterpret.rds")
+
+saveRDS(DILASSOcoefs, "results/DILASSO/DILASSOcoefs.rds")
+saveRDS(DILASSOlambda, "results/DILASSO/DILASSOlambda.rds")
+saveRDS(DILASSOnonzero, "results/DILASSO/DILASSOnonzero.rds")
+saveRDS(DILASSOsparsityRatio, "results/DILASSO/DILASSOsparsityRatio.rds")
+
+
+saveRDS(LASSOcoefs, "results/LASSO/LASSOcoefs.rds")
+saveRDS(LASSOlambda, "results/LASSO/LASSOlambda.rds")
+saveRDS(LASSOsparsityRatio, "results/LASSO/LASSOsparsityRatio.rds")
+saveRDS(LASSOnonzero, "results/LASSO/LASSOnonzero.rds")
+
+saveRDS(ENETcoefs, "results/ENET/ENETcoefs.rds")
+saveRDS(ENETalpha, "results/ENET/ENETalpha.rds")
+saveRDS(ENETlambda, "results/ENET/ENETlambda.rds")
+saveRDS(ENETsparsityRatio, "results/ENET/ENETsparsityRatio.rds")
+saveRDS(ENETcv, "results/ENET/ENETcv.rds")
+saveRDS(ENETnonzero, "results/ENET/ENETnonzero.rds")
+
+saveRDS(gLASSOcoefs, "results/gLASSO/gLASSOcoefs.rds")
+saveRDS(gLASSOlambda, "results/gLASSO/gLASSOlambda.rds")
+saveRDS(gLASSOsparsityRatio, "results/gLASSO/gLASSOsparsityRatio.rds")
+saveRDS(gLASSOnonzero, "results/gLASSO/gLASSOnonzero.rds")
+
+saveRDS(MSFEs, "results/MSFEs.rds")
