6429b2ef36f14f56b3738e78698aeb9026dd061d
Yoshiki Nakajima <kopites_1892@yahoo.co.jp>
2019-01-25 17:41:27 +0900

    update

--- saveResults.r
+++ saveResults.r
@@ -16,18 +16,14 @@ names(LASSOcoefs) <- varNameShort
 names(ENETcoefs) <- varNameShort
 names(ENETcv) <- varNameShort
 names(gLASSOcoefs) <- varNameShort
-names(SCADcoefs) <- varNameShort
 
 
 
 saveRDS(ARlags, "results/ARlags.rds")
 
 saveRDS(DIfactor, "results/DI/DIfactor.rds")
-saveRDS(DIfactorDyn, "results/DI/DIfactorDyn.rds")
-saveRDS(DIfactorR2, "results/DI/DIfactorR2.rds")
 saveRDS(DIlags,"results/DI/DIlags.rds")
 saveRDS(DIfactorList, "results/DI/DIfactorList.rds")
-saveRDS(DIinterpret, "results/DI/DIinterpret.rds")
 
 saveRDS(DICVlags, "results/DICV/DICVlags.rds")
 saveRDS(DICVfactor, "results/DICV/DICVfactor.rds")
@@ -38,31 +34,19 @@ saveRDS(DICVr2, "results/DICV/DICVr2.rds")
 saveRDS(DILASSOcoefs, "results/DILASSO/DILASSOcoefs.rds")
 saveRDS(DILASSOlambda, "results/DILASSO/DILASSOlambda.rds")
 saveRDS(DILASSOnonzero, "results/DILASSO/DILASSOnonzero.rds")
-saveRDS(DILASSOsparsityRatio, "results/DILASSO/DILASSOsparsityRatio.rds")
 saveRDS(DILASSOr2, "results/DILASSO/DILASSOr2.rds")
 
 
 saveRDS(LASSOcoefs, "results/LASSO/LASSOcoefs.rds")
 saveRDS(LASSOlambda, "results/LASSO/LASSOlambda.rds")
-saveRDS(LASSOsparsityRatio, "results/LASSO/LASSOsparsityRatio.rds")
 saveRDS(LASSOnonzero, "results/LASSO/LASSOnonzero.rds")
 
 saveRDS(ENETcoefs, "results/ENET/ENETcoefs.rds")
 saveRDS(ENETalpha, "results/ENET/ENETalpha.rds")
 saveRDS(ENETlambda, "results/ENET/ENETlambda.rds")
-saveRDS(ENETsparsityRatio, "results/ENET/ENETsparsityRatio.rds")
 saveRDS(ENETcv, "results/ENET/ENETcv.rds")
 saveRDS(ENETnonzero, "results/ENET/ENETnonzero.rds")
 
 saveRDS(gLASSOcoefs, "results/gLASSO/gLASSOcoefs.rds")
 saveRDS(gLASSOlambda, "results/gLASSO/gLASSOlambda.rds")
-saveRDS(gLASSOsparsityRatio, "results/gLASSO/gLASSOsparsityRatio.rds")
 saveRDS(gLASSOnonzero, "results/gLASSO/gLASSOnonzero.rds")
-
-saveRDS(SCADcoefs, "results/SCAD/SCADcoefs.rds")
-saveRDS(SCADlambda, "results/SCAD/SCADlambda.rds")
-saveRDS(SCADsparsityRatio, "results/SCAD/SCADsparsityRatio.rds")
-saveRDS(SCADnonzero, "results/SCAD/SCADnonzero.rds")
-
-
-
