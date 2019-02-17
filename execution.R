20fdfad296357f2bb11625838499293729f05176
Yoshiki Nakajima <kopites_1892@yahoo.co.jp>
2019-02-17 15:29:59 +0900

    add arcv
    ar model where the lag order is selected using cross validation (h-step-ahead forecast)

--- execution.R
+++ execution.R
@@ -10,7 +10,6 @@ library(grplasso)
 library(foreach)
 library(doParallel)
 registerDoParallel(detectCores())
-library(openxlsx)
 
 
 rm(list=ls()) # clear workspace
@@ -26,17 +25,18 @@ tictoc::tic()
 pb<- txtProgressBar(0,length(hChoises)*length(targetVariables), style=3)
 for (horizon in 1:length(hChoises)){
   h <- hChoises[horizon]
-  T1 <- which(index(dat)==" 7 2008") - h # end of initialisation period
-  T2 <- which(index(dat)==" 7 2013") - h # end of cv
+  T1 <- which(index(dat)=="July 2008") - h # end of initialisation period
+  T2 <- which(index(dat)=="July 2013") - h # end of cv
   for(var in 1:length(targetVariables)){
     targetVar <- targetVariables[var]
-    source("models/ar.r") # 7 mins to execute
-    source("models/di.r") # 10 mins
-    source("models/dicv.r") # 3 hrs / 40 min (iMac)
-    source("models/dilasso.r") # 1 hr / 15 mins (iMac)
-    source("models/lasso.r") # 1hr / 20 mins (lab)
-    source("models/enet.r") # 3.5-4 hrs (Lab)
-    source("models/glasso.r") # 4 hrs (iMac)
+    # source("models/ar.r") # 7 mins to execute
+    source("models/arcv.r") # 1.5 min
+    # source("models/di.r") # 10 mins
+    # source("models/dicv.r") # 3 hrs / 40 min (iMac)
+    # source("models/dilasso.r") # 1 hr / 15 mins (iMac)
+    # source("models/lasso.r") # 1hr / 20 mins (lab)
+    # source("models/enet.r") # 3.5-4 hrs (Lab)
+    # source("models/glasso.r") # 4 hrs (iMac)
     setTxtProgressBar(pb, (horizon-1)*length(targetVariables)+var)
   }
 }
@@ -59,7 +59,7 @@ names(gLASSOcoefs) <- varNameShort
 
 # Standardise MSFE--------------------------------------------------------
 MSFE2 <- lapply(1:3, function(h){
-  t(apply(MSFEs[[h]],1, function(x) x*(1/MSFEs[[h]]["AR",]))) %>% 
+  t(apply(MSFEs[[h]],1, function(x) x*(1/MSFEs[[h]]["ARCV",]))) %>% 
     set_colnames(varNameShort)
 }) %>% set_names(c("h1","h3","h12"))
 
